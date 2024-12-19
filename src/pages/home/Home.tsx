6b2f3e0a4edb72058884634aac2d78038d4483b0
nyxt4s <garaynyxmedina@gmail.com>
2024-12-19 13:58:05 -0300

    avances

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import imgPc from '../../assets/Rectangle 43 (1).svg';
 import imgPc2 from '../../assets/Group 16.svg';
 import Footer from '../../components/footer/footer';
+import cuatro from '../../assets/04.svg';
 
 export default function Home (){
 
@@ -9,7 +10,7 @@ export default function Home (){
             <div className="h-svh min-w-full bg-black">
        
                 <div className="container flex h-svh mx-auto px-4 bg-black pt-20  mb-10">
-                    <div className="w-1/2 content-center h-screen text-center justify-center items-center">
+                    <div className="w-1/2 content-center  text-center justify-center items-center">
                         <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-8xl text-white  w-max">NYX TECHNOLOGIES</h1>
                         <p className="text-white text-1xl mt-4">"Tu Tiempo Importa, Nuestro Tecnología 
                         Simplifica: ¡Vive más, Preocúpate menos!"</p>
@@ -23,9 +24,10 @@ export default function Home (){
                     </div>
                   
                 </div>
-                
                 <div className="container mx-auto px-4 h-auto py-3 mb-10 ">
-                    <h5 className='items-center text-left justify-center text-2xl pb-6'>Nuestros Servicios</h5>
+                    <div className='w-full'>
+                        <h5 className='items-center text-left justify-center text-2xl pb-6'>Nuestros Servicios</h5>
+                    </div>
                     <div className='flex justify-center'>
                         <div className='w-1/3 items-center text-center justify-center'>
                             
@@ -55,67 +57,67 @@ export default function Home (){
                     </div>
                 </div>
 
-                <div className="container mx-auto px-4 h-auto py-3 mb-10 ">
-                    <h5>Things We Enjoy</h5>
-                    <div className='flex'>
+                <div className="container mx-auto px-4 h-auto py-10 mb-10 ">
+                    <h5 className='text-2xl text-black ml-28'>Things We Enjoy</h5>
+                    <div className='flex justify-center'>
                     <div className='align-center flex-col p-4'>
-                        <div className='p-3'>
+                        <div className='p-3 flex'>
                             <div>
-                                <img src="" alt="" />
+                                <img src={cuatro} alt="" className='p-4 text-center justify-center align-center'/>
                             </div>
-                            <div>
+                            <div className='py-4'>
+                                <h6 className='text-2xl'>Content writing</h6>
+                                <p>But I must explain to you how all this mistaken 
+                                idea of denouncing pleasure</p>
+                            </div>
+                        </div>
+                        <div className='p-3 flex'>
+                        <div>
+                                <img src={cuatro} alt="" className='p-4 text-center justify-center align-center'/>
+                            </div>
+                            <div className='py-4'>
                                 <h6>Content writing</h6>
                                 <p>But I must explain to you how all this mistaken 
                                 idea of denouncing pleasure</p>
                             </div>
                         </div>
-                        <div className='p-3'>
-                            <div>
-                                <img src="" alt="" />
+                        <div className='p-3 flex'>
+                             <div>
+                                <img src={cuatro} alt="" className='p-4 text-center justify-center align-center'/>
                             </div>
-                            <div>
-                                <h6>Content writing</h6>
-                                <p>But I must explain to you how all this mistaken 
-                                idea of denouncing pleasure</p>
-                            </div>
-                        </div>
-                        <div className='p-3'>
-                            <div>
-                                <img src="" alt="" />
-                            </div>
-                            <div>
+                            <div className='py-4'>
                                 <h6>Content writing</h6>
                                 <p>But I must explain to you how all this mistaken 
                                 idea of denouncing pleasure</p>
                             </div>
                         </div>
                     </div>
-                    <div className='p-4'>
-                        <div className='p-3'>
+                    <div className='pt-12'>
+                        <div className='p-3 flex'>
                             <div>
-                                <img src="" alt="" />
+                                <img src={cuatro} alt="" className='p-4 text-center justify-center align-center'/>
                             </div>
-                            <div>
+                            <div className='py-4'>
                                 <h6>Content writing</h6>
                                 <p>But I must explain to you how all this mistaken 
                                 idea of denouncing pleasure</p>
                             </div>
                         </div>
-                        <div className='p-3'>
+                        <div className='p-3 flex'>
                             <div>
-                                <img src="" alt="" />
+                                <img src={cuatro} alt="" className='p-4 text-center justify-center align-center'/>
                             </div>
-                            <div>
+                            <div className='py-4'>
                                 <h6>Content writing</h6>
                                 <p>But I must explain to you how all this mistaken 
                                 idea of denouncing pleasure</p>
                             </div>
                         </div>
-                        <div className='p-3'>
+                        <div className='p-3 flex'>
                             <div>
-                                <img src="" alt="" />
+                                <img src={cuatro} alt="" className='p-4 text-center justify-center align-center'/>
                             </div>
-                            <div>
+                            <div className='py-4'>
                                 <h6>Content writing</h6>
                                 <p>But I must explain to you how all this mistaken 
                                 idea of denouncing pleasure</p>
