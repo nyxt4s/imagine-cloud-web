e66123adefcd8e26d812b6ba536c92896486e457
nyxt4s <garaynyxmedina@gmail.com>
2024-12-24 10:02:48 -0300

    se agrega react router dom}

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 
 import Navbar from './components/navbar/Navbar'
+import Blog from './pages/blog/Blog';
 import Home from './pages/home/Home'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 
 
   return (
-    <div className=''>
-      <Navbar></Navbar>
-      <Home></Home>
-    </div>
+    <Router>
+            <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+    </Router>
         
    
   )
